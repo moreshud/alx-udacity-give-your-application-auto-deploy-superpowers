c3741e7e528ed570dfd7ed732598f3b805128bd7
moreshud <mo.yekini@gmail.com>
2022-08-06 22:45:01 +0100

    overall migrations fixes

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,9 +1,9 @@
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST=database-1.cwmal92kt4eq.us-east-1.rds.amazonaws.com
+export TYPEORM_HOST=postgres01.cwmal92kt4eq.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
-export TYPEORM_PASSWORD='*admin_01#'
-export TYPEORM_DATABASE=database-1
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_DATABASE=postgres
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 export TYPEORM_MIGRATIONS_DIR=./src/migrations
